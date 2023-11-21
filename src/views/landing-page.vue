6bed7f6bba539cb64a364fad7e7c99da5203204e
Aaron Evans <Aaron.Evans@iceland.co.uk>
2023-11-21 16:05:22 +0000

    update landing page

--- src/views/landing-page.vue
+++ src/views/landing-page.vue
@@ -1,10 +1,21 @@
+<script>
+export default {
+  name: 'App'
+  // You can add data, methods, computed properties, etc. here
+};
+</script>
+
 <template>
-    <v-app>
-        <v-main>
-        <v-container>
-            <h1>Hello Vuetify</h1>
-        </v-container>
-        </v-main>
-    </v-app>
-</template>
+    <div id="app">
+      <header>
+        <h1>Welcome to My Vue.js App</h1>
+      </header>
+      <main>
+        <p>This is a basic Vue.js landing page.</p>
+      </main>
+      <footer>
+        <p>© 2023 Your Name</p>
+      </footer>
+    </div>
+  </template>
   
