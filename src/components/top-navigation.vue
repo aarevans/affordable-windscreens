31bf1d85af74e1704dba4039b2b53ed3ae9d8c39
aaroniceland <102594470+aarevans@users.noreply.github.com>
2024-02-09 20:48:06 +0000

    tweaked nav buttons

--- src/components/top-navigation.vue
+++ src/components/top-navigation.vue
@@ -2,16 +2,23 @@
   <v-app-bar app color="indigo" dark scroll-behavior="collapse">
     <v-spacer></v-spacer> <!-- Spacer before buttons -->
 
-    <router-link to="/" tag="v-btn" text>Home</router-link>
-    <router-link to="/about" tag="v-btn" text>About</router-link>
-    <router-link to="/contact" tag="v-btn" text>Contact</router-link>
+    <!-- Using router-links instead of plain buttons -->
+    <router-link to="/" custom v-slot="{ navigate }">
+      <v-btn text @click="navigate">Home</v-btn>
+    </router-link>
+    <router-link to="/about" custom v-slot="{ navigate }">
+      <v-btn text @click="navigate">About</v-btn>
+    </router-link>
+    <router-link to="/contact" custom v-slot="{ navigate }">
+      <v-btn text @click="navigate">Contact</v-btn>
+    </router-link>
 
     <v-spacer></v-spacer> <!-- Spacer after buttons -->
   </v-app-bar>
 
   <v-img :src="banner" height="125" cover ></v-img>
-
 </template>
+
 
 <script setup>
 
